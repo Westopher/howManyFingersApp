f8de328b171e3c60da02dba0d384c7e85c5eaed8
West Kraemer <WestKraemer@gmail.com>
2016-12-04 11:56:18 -0500

    created user interface including text field, button, and images.

--- howManyFingersApp/ViewController.swift
+++ howManyFingersApp/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let randomNumber = arc4random_uniform(6)
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
